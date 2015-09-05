ffd74a414ab12dc9195d215d10274f0b8c740392
Alex <abosch19@gmail.com>
2015-09-05 03:24:01 +0200

    Aprendiendo el BOM(Browser Object Model) y practicando la geolocalización

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-alert("Hola Mundo");
+(function() {
+
+if(navigator.geolocation) {
+
+	navigator.geolocation.getCurrentPosition(getCoords, errorFound);
+
+} else {
+	alert("Por favor, actualiza tu navegador");
+}
+
+function errorFound (err) {
+	alert("Ocurrió un error: " + err.message);
+	//0: Error desconocido
+	//1: Permiso denegado
+	//2: Posición no está disponible
+	//3: Timeout
+}
+
+function getCoords (position) {
+	var lat = position.coords.latitude;
+	var lon = position.coords.longitude;
+	console.log("Tu posición es: " + lat + "," + lon);
+}
+
+})();
